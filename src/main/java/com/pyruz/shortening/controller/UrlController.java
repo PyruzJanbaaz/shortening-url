12de3aab795a66c13df168c3fa3988afb6a99040
pyruz <pyruz.janbaaz@gmail.com>
2022-11-16 09:29:50 +0400

    bug fix

--- src/main/java/com/pyruz/shortening/controller/UrlController.java
+++ src/main/java/com/pyruz/shortening/controller/UrlController.java
@@ -2,7 +2,6 @@ package com.pyruz.shortening.controller;
 
 import com.pyruz.shortening.model.domain.CustomUrlBean;
 import com.pyruz.shortening.model.domain.UrlBean;
-import com.pyruz.shortening.model.dto.base.BaseDTO;
 import com.pyruz.shortening.service.impl.UrlService;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.http.HttpStatus;
@@ -25,26 +24,65 @@ public class UrlController {
         this.urlService = urlService;
     }
 
+    /**
+     * Generate ShortURL.
+     * <p>
+     * This method creates and returns the stored url.
+     *
+     * @param  urlBean  the url
+     * @return Url in baseDto data field
+     */
     @PostMapping("/url")
     public ResponseEntity<?> generateShortURL(@Valid @RequestBody UrlBean urlBean) {
         return new ResponseEntity<>(urlService.generateShortURL(urlBean), HttpStatus.CREATED);
     }
 
+    /**
+     * Generate custom ShortURL.
+     * <p>
+     * This method creates and returns the stored url.
+     *
+     * @param  customUrlBean  the url and custom portion
+     * @return Url in baseDto data field
+     */
     @PostMapping("/url/custom")
     public ResponseEntity<?> generateCustomShortURL(@Valid @RequestBody CustomUrlBean customUrlBean) {
         return new ResponseEntity<>(urlService.generateCustomShortURL(customUrlBean), HttpStatus.OK);
     }
 
+    /**
+     * Redirect to the originalURL.
+     * <p>
+     * This method redirects to the originalURL.
+     *
+     * @param  shortURL  the shortURL to convert to the originalURL
+     */
     @GetMapping("/url")
     public void redirectToOriginalUrl(@URL @RequestParam String shortURL, HttpServletResponse response) throws IOException {
         response.sendRedirect(urlService.getCurrentURL(shortURL).getOriginalURL());
     }
 
+    /**
+     * Yesterday reviews by ShortURL.
+     * <p>
+     * This method returns the reviews of an url.
+     *
+     * @param  shortURL  the shortURL to get the yesterday reviews
+     * @return List<Review> in baseDto data field
+     */
     @GetMapping("/url/review")
     public ResponseEntity<?> getUrlReviewCount(@URL @RequestParam String shortURL) {
         return new ResponseEntity<>(urlService.getURLReview(shortURL), HttpStatus.OK);
     }
 
+    /**
+     * Delete a ShortURL.
+     * <p>
+     * This method removes the url.
+     *
+     * @param  shortURL  the shortURL to remove from database
+     * @return baseDto
+     */
     @DeleteMapping("/url")
     public ResponseEntity<?> redirectToOriginalUrl(@URL @RequestParam String shortURL) {
         return new ResponseEntity<>(urlService.deleteURL(shortURL), HttpStatus.OK);
